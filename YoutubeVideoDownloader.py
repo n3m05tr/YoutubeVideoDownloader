7d16cd76ac79ac7885135d38ae99eec65c9522c0
Nemanja Stracenski <nemanjastracenski2005@gmail.com>
2021-08-09 00:35:13 +0200

    Add files via upload

---
+++ YoutubeVideoDownloader.py
@@ -0,0 +1,34 @@
+import time
+import colorama
+from colorama import Fore, Back, Style
+from pytube import YouTube
+link = input(Fore.RED + Back.WHITE + "Paste the link here: ")
+yt = YouTube(link)
+
+try:
+    ys = yt.streams.get_lowest_resolution()
+except:
+    print(Fore.RED + "Connection Error")
+
+print(Fore.BLUE + "\nTitle: ", yt._title)
+print(Fore.BLUE + "Length: ", yt.length, "seconds")
+print(Fore.BLUE + "Rating:", yt.rating)
+
+def DownloadConfirmation():
+    print(Fore.RED + Back.WHITE + "\nYou are sure about dowloading this video?")
+    SureNot = input(Fore.RED + Back.WHITE + "Yes or No?: ")
+    if (SureNot == "Yes" or "yes"):
+        SureNot_Bool = True
+    elif (SureNot == "No" or "no"):
+        SureNot_Bool = False
+    if SureNot_Bool == True:
+        ys.download()
+        print(Fore.BLUE + "\nDownloading...")
+        print(Fore.BLUE + "Download Complete")
+    elif SureNot_Bool == False:
+        print(Fore.BLUE + "Ok then, bye!")
+        time.sleep(3)
+
+DownloadConfirmation()
+
+time.sleep(4)
